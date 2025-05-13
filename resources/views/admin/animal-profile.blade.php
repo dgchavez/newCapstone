15c7182f942db9c3338dc7ecd53f99c70bfba638
degchavez <s.chavez.daveedward@cmu.edu.ph>
2025-05-13 14:05:59 +0800

    updates

--- resources/views/admin/animal-profile.blade.php
+++ resources/views/admin/animal-profile.blade.php
@@ -559,7 +559,7 @@
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
+
     <script>
         const transactionSubtypes = @json($transactionSubtypes);
 
